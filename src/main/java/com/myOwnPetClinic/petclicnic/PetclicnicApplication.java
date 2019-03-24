e8924c2ba77aaf3c554aec668bbc409b320c8462
Karol Marczyk <KarMarcz@gmail.com>
2019-03-24 16:09:15 +0100

    First Commit

---
+++ src/main/java/com/myOwnPetClinic/petclicnic/PetclicnicApplication.java
@@ -0,0 +1,13 @@
+package com.myOwnPetClinic.petclicnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclicnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclicnicApplication.class, args);
+	}
+
+}
